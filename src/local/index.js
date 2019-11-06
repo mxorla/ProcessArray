0e513ec58bfa29f0004030ef38ad5dbf09ae98c5
Matias <matias.orlandella@gmail.com>
2019-11-05 21:06:43 -0300

    change message

--- src/local/index.js
+++ src/local/index.js
@@ -7,7 +7,7 @@ main();
 async function main() {
     const array = generate({ size: 10000000 });
     const chunk = 10000;
-    console.log("Processing array with 10 millions elements on aws lambda");
+    console.log("Processing array with 10 millions elements on local");
     const allResponses = await process({ array, lambda, chunk });
     console.log(`Showing 10 of ${allResponses.length} elements: `, allResponses.slice(0, 10));
 }
